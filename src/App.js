807512ac253673704a38804c377b0727778ef159
alexo <alexola.22@gmail.com>
2022-09-15 10:31:39 +0100

    Creating the bottom component with the style and parameters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnChange} />
-      
+      <CurrentWeather />      
     </div>
   );
 }
